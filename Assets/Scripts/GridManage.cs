0bfbd78c71bae888826d49554aafb5715c26da62
Jen2442 <collazojennifer22@yahoo.com>
2025-08-19 20:04:48 -0700

    Set

---
+++ Assets/Scripts/GridManage.cs
@@ -0,0 +1,125 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GridManage : MonoBehaviour
+{
+    [SerializeField] public int _width, _height, _RailsHeight,_RailsWidth; //creates assets in the inspector
+
+    [SerializeField] private GameObject _spherePrefab;//creates asset in the inspector
+
+    [SerializeField] private Vector3 _gridOffset = Vector3.zero;//change to start grid at set corrdinates
+
+    [SerializeField] private Vector3 _RailOffset = Vector3.zero;//change to start rails at set coordinates
+    
+
+         
+
+
+
+    void Start()
+    {
+        generateGrid();// spawns grid
+
+        
+
+    }
+
+    void generateGrid()
+    {
+
+        float PowerRailSpacing = 3.0f;
+        
+        float _FirstTerminal = 0.08847f;
+
+        float RowGap = 0.0422f;
+        float temp = 20.0f;
+
+
+
+        for (int x = 0; x < _width; x++)//loop for width
+        {
+
+            for (int y = 0; y < _height; y++)//loop for height 
+            {
+                float TerminalSpacing = 0.02286f;
+                float HeightSpacing = 0.0235f;
+                float BridgeSpace = 0.211f;
+
+
+                float Ypos = y * TerminalSpacing;
+                float zPos = x * HeightSpacing;
+
+
+                float PowerRailVerticalOffset = 0.4f;
+
+
+                if (x >= 5) zPos += RowGap;
+
+                if (x >= 10) zPos += BridgeSpace;
+                if (x >= 15) zPos += RowGap;
+
+
+                Vector3 spawnPos = new Vector3(Ypos, 0.0f, zPos)+_gridOffset;
+                var sphere = Instantiate(_spherePrefab, spawnPos, Quaternion.identity, transform);
+                sphere.name = $"({x},{y})";
+
+                var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
+                Tiles tiles = sphere.GetComponent<Tiles>();
+                tiles.Init(isOffset);
+
+
+
+
+            }
+        }
+
+
+
+
+        for (int x = 0; x < _RailsWidth; x++)
+        {
+            for (int y = 0; y < _RailsHeight; y++)
+            {
+
+                float TerminalSpacing = 0.02286f;
+                float HeightSpacing = 0.0235f;
+
+                float zPos = x * TerminalSpacing;
+
+                float Ypos = y* HeightSpacing;
+
+                float CenterRail = 0.36321f;
+                float SpaceCenter = 0.0332f;
+
+                if (x >= 2) zPos += CenterRail;
+                if (x >= 4) zPos += SpaceCenter;
+                if (x >= 6) zPos += CenterRail;
+
+
+
+                int groupCount = y / 5;
+                if (groupCount > 0 && groupCount <= 50)
+                {
+                    Ypos += groupCount * 0.0188f; // adjust gap per group 0.02700 0.04986
+                }
+
+
+                Vector3 spawnPos = new Vector3(Ypos, 0.0f, zPos)+_RailOffset;
+                var sphere = Instantiate(_spherePrefab, spawnPos, Quaternion.identity, transform);
+                sphere.name = $"({x},{y})";
+
+                var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
+                Tiles tiles = sphere.GetComponent<Tiles>();
+                tiles.Init(isOffset);
+            }
+        }
+
+    
+
+
+       
+    }
+
+    
+}
---
+++ Assets/Scripts/TileSocketListener.cs
@@ -0,0 +1,40 @@
+
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TileSocketListener : MonoBehaviour
+{
+    private XRSocketInteractor socket;
+
+    void Awake()
+    {
+        socket = GetComponent<XRSocketInteractor>();
+        socket.selectEntered.AddListener(OnSocketed);
+        socket.selectExited.AddListener(OnUnsocketed);
+    }
+
+    void OnDestroy()
+    {
+        socket.selectEntered.RemoveListener(OnSocketed);
+        socket.selectExited.RemoveListener(OnUnsocketed);
+    }
+
+    private void OnSocketed(SelectEnterEventArgs args)
+    {
+        GameObject socketObj = gameObject;
+        GameObject insertedObj = args.interactableObject.transform.gameObject;
+
+        TilesManage.Instance.IncrementSocketUsage(socketObj);
+        Debug.Log($"{socketObj.name} now socketed with {insertedObj.name}");
+    }
+
+    private void OnUnsocketed(SelectExitEventArgs args)
+    {
+        GameObject socketObj = gameObject;
+        GameObject removedObj = args.interactableObject.transform.gameObject;
+
+        TilesManage.Instance.DecrementSocketUsage(socketObj);
+        Debug.Log($"{socketObj.name} unsocketed from {removedObj.name}");
+    }
+}
+
