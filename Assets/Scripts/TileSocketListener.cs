583d569fadebe603bf6b099760c4588d988e8d09
Jen2442 <collazojennifer22@yahoo.com>
2025-08-25 21:13:36 -0700

    LED's

--- Assets/Scripts/TileSocketListener.cs
+++ Assets/Scripts/TileSocketListener.cs
@@ -37,7 +37,7 @@ private void OnSocketed(SelectEnterEventArgs args)
 
     TilesManage.Instance.IncrementSocketUsage(socketObj);
         Debug.Log($"{socketCoordName},{insertedObj}");
-        
+
         TilesManage.Instance._Connections.Add(TilesManage.Instance.GetOrderedPair(socketCoordName, insertedObj.name));/////////////////////
 
     //Debug.Log($"Socket {socketCoordName} now socketed with {insertedObjCoordName}");
@@ -64,7 +64,7 @@ private void OnSocketed(SelectEnterEventArgs args)
 
 
         TilesManage.Instance.DecrementSocketUsage(socketObj);
-        Debug.Log($"{socketObj.name} unsocketed from {removedObj.name}");
+        //Debug.Log($"{socketObj.name} unsocketed from {removedObj.name}");
         
         TilesManage.Instance._Connections.Remove(TilesManage.Instance.GetOrderedPair(socketCoordName, removedObj.name));
 }
